b0cddc49e1d9e122d46c6809c07e26d421b0942f
Vladimir Sierra <vladimirsierra@ciencias.unam.mx>
2021-02-14 13:52:03 -0600

    Add documentation to movements.pl

--- src/BeeColony/movements.pl
+++ src/BeeColony/movements.pl
@@ -1,3 +1,8 @@
+/*
+ * valid_move( Visited, Pos_inicial, Move_id, N, (X,Y) ).
+ * Predicado que es true si Visited son las casillas visitadas hasta ese momento, Pos_inicial la posicion actual de la
+ * pieza, N la longitud del lado del tablero, y (X,Y) es una posicion final valida aplicando el movimiento Move_id
+*/
 valid_move(Visited, PosInicial, MoveId ,N,  (X,Y) ):-
     move_pattern(MoveId, PosInicial, (X,Y)),
     X >= 1,
@@ -6,41 +11,41 @@ valid_move(Visited, PosInicial, MoveId ,N,  (X,Y) ):-
     Y =< N,
     not(member((X,Y), Visited)).
 
-%- Predicado que es true si PosFinal es la posicion final de la pieza al aplicar el movimento con id 1
-move_pattern(1, (X,Y) , PosFinal):-
+
+/*
+ * move_pattern( K , (X,Y), Pos_final
+ * Predicado que es true si Pos_final es la posicion final de la pieza al aplicar el movimento con id K
+ * empezando en la posicion (X,Y).
+*/
+move_pattern(1, (X,Y) , Pos_final):-
     X2 is X + 1,
     Y2 is Y - 2,
-    PosFinal = (X2, Y2).
-
-move_pattern(2, (X, Y), PosFinal):-
+    Pos_final = (X2, Y2).
+move_pattern(2, (X, Y), Pos_final):-
     X2 is X + 2,
     Y2 is Y - 1,
-    PosFinal = (X2, Y2).
-
-
-move_pattern(3, (X, Y), PosFinal):-
+    Pos_final = (X2, Y2).
+move_pattern(3, (X, Y), Pos_final):-
     X2 is X + 2,
     Y2 is Y + 1,
-    PosFinal = (X2, Y2).
-
-move_pattern(4, (X, Y), PosFinal):-
+    Pos_final = (X2, Y2).
+move_pattern(4, (X, Y), Pos_final):-
     X2 is X + 1,
     Y2 is Y + 2,
-    PosFinal = (X2, Y2).
-
-move_pattern(5, (X, Y), PosFinal):-
+    Pos_final = (X2, Y2).
+move_pattern(5, (X, Y), Pos_final):-
     X2 is X - 1,
     Y2 is Y + 2,
-    PosFinal = (X2, Y2).
-move_pattern(6, (X, Y), PosFinal):-
+    Pos_final = (X2, Y2).
+move_pattern(6, (X, Y), Pos_final):-
     X2 is X - 2,
     Y2 is Y + 1,
-    PosFinal = (X2, Y2).
-move_pattern(7, (X, Y), PosFinal):-
+    Pos_final = (X2, Y2).
+move_pattern(7, (X, Y), Pos_final):-
     X2 is X - 2,
     Y2 is Y - 1,
-    PosFinal = (X2, Y2).
-move_pattern(8, (X, Y), PosFinal):-
+    Pos_final = (X2, Y2).
+move_pattern(8, (X, Y), Pos_final):-
     X2 is X - 1,
     Y2 is Y - 2,
-    PosFinal = (X2, Y2).
+    Pos_final = (X2, Y2).
