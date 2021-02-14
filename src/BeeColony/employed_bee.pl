b89e381248e67facceac2136980ced3ccfe2bf57
Vladimir Sierra <vladimirsierra@ciencias.unam.mx>
2021-02-14 06:59:07 -0600

    Add documentation

--- src/BeeColony/employed_bee.pl
+++ src/BeeColony/employed_bee.pl
@@ -1,5 +1,10 @@
 :-[compute].
-%- Predicado que es true si N es un numero natural y Z es la lista que guarda la representacion de una fuente de comida.
+
+/*
+ * generate_employed_bees(K, N, Z).
+ * Predicado que es true si N es un numero natural que sera la longitud del tablero,  Z es la lista que guarda la
+ * representacion de K fuentes de comida.
+*/
 generate_employed_bees(1, N, Z):-
     random_init(N, Z1),!,
     Z = [Z1].
@@ -11,7 +16,22 @@ generate_employed_bees(Num_emp_bees, N, Z):-
     random_init(N, Z2),
     Z = [Z2 | Z1].
 
+/*
+ * employed_bees_job(L, N, Pos_inicial, Z).
+ * Predicado que es true si L es la lista de fuentes de comida, N la longitud del lado del tablero,
+ * Pos_inicial es la posicion de inicio del recorrido y Z es el resultado de que todas las abejas empleadas
+ * realicen su tarea.
+*/
+employed_bees_job(L, N, Pos_inicial,  Z):-
+    Longitud is N * N - 1,
+    update_all_solutions(L,L,N, Longitud,Pos_inicial, Z).
 
+
+/*
+ * merge_solution(S1, S2, L, Z).
+ * Predicado que es true si S1 y S2 son listas de patrones de movimientos, L es la longitud de S1 y S2, y Z es
+ * el resultado de mezclar S1 con S2 con la tecnica Three point switch.
+*/
 merge_solution(Old_solution, Neighboring_solution, Longitud, New_solution):-
     %- primero generamos los tres puntos random de corte
     generate_cut_points(Longitud, [P1,P2,P3]),
@@ -28,25 +48,35 @@ merge_solution(Old_solution, Neighboring_solution, Longitud, New_solution):-
     append([Segmento1, Segmento2, Segmento3, Segmento4], New_solution).
 
 
-
+/*
+ * generate_cut_point(L, Z).
+ * Predicado que es true si Z es una lista de tres puntos diferentes ordenados de menor a mayor
+ * que se encuentran en un rango [1,L].
+*/
 generate_cut_points(Longitud, Z):-
     random(1, Longitud, R1),
     random(1, Longitud, R2),
     random(1, Longitud, R3),
-    (   R1 \= R2,
+    (
+        %- ordenamos los puntos de ser necesario
+        R1 \= R2,
         R2 \= R3,
         R1 \= R3 -> sort([R1,R2,R3],Z) ;
+        %- en caso de obtener puntos iguales repetimos hasta obtener diferentes
         generate_cut_points(Longitud, Z)
     ).
 
 
-employed_bees_job(L, N, Pos_inicial,  Z):-
-    Longitud is N * N - 1,
-    update_all_solutions(L,L,N, Longitud,Pos_inicial, Z).
-
+/*
+ * update_all_solutions(L1, L2, N, Longitud, Pos_inicial, Z).
+ * Predicado que es true si L1 es la lista de secuencias a modificar, L2 es la lista
+ * de todas las secuencias elegibles para mutar, Longitud es la
+*/
 update_all_solutions([],_, _, _, _, []).
 update_all_solutions([H|T], Complete_list, N, Longitud, Pos_inicial,  Z):-
+    %- llamamos recursivamente
     update_all_solutions(T, Complete_list, N, Longitud, Pos_inicial, Z1),
+    %- evitamos que se elijan a si mismas
     delete_one( H, Complete_list, L1),
     random_member(X, L1),
     %-- hacemos el merge de ambas secuencias y decidimos greedy
